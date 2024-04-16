7231494cc03c9ffb64cb174cc6c00812b99837eb
BeeJoy-1 <beejoy3000@gmail.com>
2024-04-16 12:08:40 +0600

    did the validation

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,53 @@ function App() {
  
   const db = getDatabase();
 
-  let [name,setName] = useState()
-  let [email,setEmail] = useState()
-  let [roll,setRoll] = useState()
-  let [box,setBox] = useState()
+  let [text,setText] = useState({
+    firstname: "",
+    email: "",
+    roll: "",
+    box: ""
+  })
+
+  let [error,setError] = useState({
+    firstname: "",
+    email: "",
+    roll: "",
+    box: ""
+  })
+
   let [boy,setBoy] = useState()
   let [girl,setGirl] = useState()
   let [show,setShow] = useState(false)
+  
 
-  // useEffect(() => {
-  //   const formRef = ref(db, "Form/");
-  //   onValue(formRef, (snapshot) => {
-  //     const box = snapshot.val();
-  //   })
-  // })
+  let handleform = (e) => {
+    let {name,value} = e.target
+    setText({...text, [name]: value})
+  }
 
   let handleSubmit = () => {
-    set(push(ref(db, "Form")),{
-      user: name,
-      mail: email,
-      ID : roll,
-      School: box,
-    })
-
-    setName("")
-    setEmail("")
-    setRoll("")
-    setBoy("")
-    setGirl("")
-    setBox("")
-    setShow(true)
+    if(!text.firstname){
+      setError({firstname: "Enter Your Name !"})
+    }else if(!text.email) {
+      setError({email: "Enter Your Email !"})
+    }else if(!text.roll){
+      setError({roll: "Enter Your Roll !"})
+    }else if(!text.box){
+      setError({box: "Enter Your School !"})
+    }
+    else {
+      set(push(ref(db, "Form")),{
+        user: text.firstname,
+        mail: text.email,
+        ID : text.roll,
+        School: text.box,
+      })
+      setBoy("")
+      setGirl("")
+      setShow(true)
+      setError("")
+      setText("")
+    }
   }
 
   return (
@@ -49,10 +66,13 @@ function App() {
           <div className="drop-shadow-md shadow-lg w-[600px] mx-auto mt-[150px]">
             <h1 className="text-center text-[40px] italic ">Form Practice</h1>
             <div className="pb-[40px] pt-[20px] px-[50px] flex flex-col">
-              <input onChange={(e) => setName(e.target.value)} value={name} className="mb-[10px] p-[8px] w-[500px] px-[10px] outline-blue-300 text-[#959595]" type="text" placeholder="Name" />
-              <input onChange={(e) => setEmail(e.target.value)} value={email} className="mb-[10px] p-[8px] w-[500px] px-[10px] outline-blue-300 text-[#959595]" type="email" placeholder="Email" />
-              <input onChange={(e) => setRoll(e.target.value)} value={roll} className="mb-[10px] p-[8px] w-[500px] px-[10px] outline-blue-300 text-[#959595]" type="number" placeholder="Roll" />
-              <select onChange={(e) => setBox(e.target.value)} value={box} className=" mb-[8px] w-[500px] h-[40px] px-[5px] outline-blue-300 text-[#959595]" name="" id="">
+              <input onChange={handleform} name="firstname"  className="mb-[10px] p-[8px] w-[500px] px-[10px] outline-blue-300 text-[#959595]" type="text" placeholder="Name" />
+              <p className="text-[red] pb-[5px]">{error.firstname}</p>
+              <input onChange={handleform} name="email"  className="mb-[10px] p-[8px] w-[500px] px-[10px] outline-blue-300 text-[#959595]" type="email" placeholder="Email" />
+              <p className="text-[red] pb-[5px]">{error.email}</p>
+              <input onChange={handleform} name="roll"  className="mb-[10px] p-[8px] w-[500px] px-[10px] outline-blue-300 text-[#959595]" type="number" placeholder="Roll" />
+              <p className="text-[red] pb-[5px]">{error.roll}</p>
+              <select onChange={handleform} className=" mb-[8px] w-[500px] h-[40px] px-[5px] outline-blue-300 text-[#959595]" name="box" id="">
                 <option value="">Choose Your School...</option>
                 <option value="MGSC">MGSC</option>
                 <option value="SCPSC">SCPSC</option>
@@ -61,6 +81,7 @@ function App() {
                 <option value="Savar Model">Savar Model</option>
                 <option value="none">Others....</option>
               </select>
+              <p className="text-[red] pb-[5px]">{error.box}</p>
               <div className="flex flex-col justify-center gap-y-[5px] mx-[10px] mt-[20px]">
                 <h2 className="text-[20px]">Your Gender -</h2>
                 <div className="flex gap-x-[5px]">
